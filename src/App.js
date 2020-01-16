745addb6149425beadfa46844fc16d269e0f9443
reducks3000 <reducks3000@gmail.com>
2020-01-15 23:09:53 -0600

    added dependencies to app.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Score from './components/Score';
+import characters from './characters.json';
+import './App.css';
+import { HashRouter } from 'react-router-dom';
+
+class App extends Component {
+
+}
+
+export default App;
