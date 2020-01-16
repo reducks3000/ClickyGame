c43d8e9cc640dc76da9ff41a6cb3014e72bb8071
reducks3000 <reducks3000@gmail.com>
2020-01-16 02:38:24 -0600

    added function to shuffle character arrays being displayed to player and added check states for guesses

--- src/App.js
+++ src/App.js
@@ -1,51 +1,98 @@
 import React, {Component} from 'react';
+import CharacterImages from './components/Characters/CharacterImages';
+import Nav from './components/Nav';
 import Score from './components/Score';
 import characters from './characters.json';
 import './App.css';
 import { HashRouter } from 'react-router-dom';
-import Modal from 'react-modal';
-import Nav from './components/Navbar'
 
 class App extends Component {
 
   state = {
     characters: characters,
-    beenPicked: [],
+    pickedCharacters: [],
     topScore: 0,
+    message: "",
   }
 
-  isPicked = e => {
-    const name = e.target.attributes.getNamedItem('name').value
-    console.log(e.target.attributes.getNamedItem('name').value)
+  isPicked = event => {
+    const name = event.target.attributes.getNamedItem('name').value
+    console.log(event.target.attributes.getNamedItem('name').value)
     console.log(this.state)
     this.shuffleCharacters();
     this.checkGuess(name, this.updateTopScore);
+  }
+
+  updateTopScore = (newState, cb) => {
+    if (newState.pickedCharacters.length > newState.topScore) {
+      newState.topScore++;
+      this.setState({ topScore: newState.topScore })
+    }
+    cb(newState);
   }
 
   shuffleCharacters = () => {
     this.setState({characters: this.shuffleArray(this.state.characters)})
   }
 
-  
+    shuffleArray = (array) => {
+    let counter = array.length;
+    while (counter > 0) {
+      let index = Math.floor(Math.random() * counter);
+      counter--;
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+    return array;
+  };
+
+
+  checkGuess = (name, cb) => {
+    const newState = { ...this.state };
+    if (newState.pickedCharacters.includes(name)) {
+      newState.message = `You already chose that character. Game Over!`
+      newState.pickedCharacters = []
+      alert(`You already chose that character. Game Over!`)
+      this.setState({ message: newState.message})
+      this.setState({ pickedCharacters: []})
+    } else {
+      newState.pickedCharacters.push(name);
+      this.setState({ pickedCharacters: newState.pickedCharacters })
+    }
+    cb(newState, this.checkWin)
+  }
+
+  checkWin = (newState) => {
+    if(newState.pickedCharacters.length === 12) {
+      newState.message = `You win! Congratulations`
+      alert(`You win! Congratulations`)
+      newState.pickedCharacters = [];
+      this.setState({ message: newState.message})
+      this.setState({ pickedCharacters: [] });
+    }
+  }
 
   render() {
     return (
       <HashRouter basename='/'>
         <div>
-          <Nav>
-            <div className="container">
-              <div className="row scoreInfo">
-                <div className="col l4 m8 s12">
-                  <h3>Try not to click the same character twice!</h3>
-                </div>
+          <Nav/>
+          <div className="container">
+            <div className="row score-info valign-wrapper">
+              <div className="col l6 m8 s12">
+                <h4>Try to not click the same character twice!</h4>
+              </div>
+              <div className="col l6 m4 s12 center">
+                <Score type="Score" score={this.state.pickedCharacters.length}/>
+                <Score type="Top Score" score={this.state.topScore}/>
               </div>
             </div>
-          </Nav>
+          </div>
         </div>
       </HashRouter>
     )
   }
-
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = props => {
+    return (
+        <nav className="teal lighten-2">
+            <div className="valign-wrapper">
+                <p className="brand-logo center">Clicky Game!</p>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
