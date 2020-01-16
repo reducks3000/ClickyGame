2da7f0ebeba935f166f848c502ac6fb0df2a1bef
reducks3000 <reducks3000@gmail.com>
2020-01-16 01:45:35 -0600

    added function for when player selects character

--- src/App.js
+++ src/App.js
@@ -3,8 +3,48 @@ import Score from './components/Score';
 import characters from './characters.json';
 import './App.css';
 import { HashRouter } from 'react-router-dom';
+import Modal from 'react-modal';
+import Nav from './components/Navbar'
 
 class App extends Component {
+
+  state = {
+    characters: characters,
+    beenPicked: [],
+    topScore: 0,
+  }
+
+  isPicked = e => {
+    const name = e.target.attributes.getNamedItem('name').value
+    console.log(e.target.attributes.getNamedItem('name').value)
+    console.log(this.state)
+    this.shuffleCharacters();
+    this.checkGuess(name, this.updateTopScore);
+  }
+
+  shuffleCharacters = () => {
+    this.setState({characters: this.shuffleArray(this.state.characters)})
+  }
+
+  
+
+  render() {
+    return (
+      <HashRouter basename='/'>
+        <div>
+          <Nav>
+            <div className="container">
+              <div className="row scoreInfo">
+                <div className="col l4 m8 s12">
+                  <h3>Try not to click the same character twice!</h3>
+                </div>
+              </div>
+            </div>
+          </Nav>
+        </div>
+      </HashRouter>
+    )
+  }
 
 }
 
