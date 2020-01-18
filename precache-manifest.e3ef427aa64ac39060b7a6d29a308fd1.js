d2461c24b6476fead745b10be4dc9749d1777cdd
Chris <reducks3000@gmail.com>
2020-01-18 00:48:39 -0600

    Updates

---
+++ precache-manifest.e3ef427aa64ac39060b7a6d29a308fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be20a22ba5330493d002bd9d0a07ee39",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "29633e26550cc6f58124",
+    "url": "/ClickyGame/static/css/main.314fbbf3.chunk.css"
+  },
+  {
+    "revision": "d5df7c67e6c48787b959",
+    "url": "/ClickyGame/static/js/2.fe22457a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ClickyGame/static/js/2.fe22457a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29633e26550cc6f58124",
+    "url": "/ClickyGame/static/js/main.3d33f84c.chunk.js"
+  },
+  {
+    "revision": "c5ff570fb451c3db5c59",
+    "url": "/ClickyGame/static/js/runtime-main.3a261c6c.js"
+  }
+]);
